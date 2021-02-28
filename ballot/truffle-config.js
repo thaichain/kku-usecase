e2c7434f88da64baa47aeb772f50aa2283cc4f4a
Rati Montreewat <ratixoxo@gmail.com>
2021-02-28 23:26:36 +0700

    Add Doc about deployment && modify truffle-config for ballot

--- ballot/truffle-config.js
+++ ballot/truffle-config.js
@@ -1,8 +1,19 @@
 // require('babel-register');
 // require('babel-polyfill');
+require('dotenv').config();
 
-const PrivateKeyProvider = require("truffle-privatekey-provider");
+// const PrivateKeyProvider = require("truffle-privatekey-provider");
 const pkey = process.env.PKEY;
+
+
+const HDWalletProvider = require('@truffle/hdwallet-provider'); 
+const provider = new HDWalletProvider({
+  privateKeys: [pkey],
+  providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+});
+
+
+
 module.exports = 
 {
     networks: 
@@ -14,7 +25,8 @@ module.exports =
 	   		network_id: "*" // Match any network id
 		},
 		bscTestnet: {
-			provider: () => new PrivateKeyProvider(pkey, 'https://rpc-bsc-testnet.8api.sh', 0, 10),
+			// provider: () => new PrivateKeyProvider(pkey, 'https://rpc-bsc-testnet.8api.sh', 0, 10),
+			provider: () => provider,
 			network_id: 97,
 			confirmations: 2,
 			timeoutBlocks: 200,
